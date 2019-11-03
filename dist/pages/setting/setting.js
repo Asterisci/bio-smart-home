5aaf1fdf3ffe43b7bc2db665e87afaa0f1f960bd
asterisci <fanguofan@stu.xjtu.edu.cn>
2019-11-03 13:21:24 +0800

    完成界面设计

---
+++ dist/pages/setting/setting.js
@@ -0,0 +1,95 @@
+// pages/setting.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [{
+        id: 'auto',
+        name: 'auto module',
+        open: true,
+        pages: ['button', 'list', 'slideview', 'input', 'slider', 'uploader']
+      },
+      {
+        id: 'manual',
+        name: 'mauual module',
+        open: false,
+        pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loading', 'loadmore', 'panel', 'preview', 'progress']
+      }
+    ],
+    power: 1,
+    aromaType:['xx','xxx'],
+    aroma: 0
+  },
+  kindToggle: function(e) {
+    var id = e.currentTarget.id,
+      list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
